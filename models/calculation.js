21c18b860a77f4546c22e00f9a78d67b7f18ac99
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-01-21 15:05:53 +0530

    added models

---
+++ models/calculation.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const calculationSchema = new mongoose.Schema({
+    expression: {
+        type: String,
+        required: true
+    },
+    result: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Calculation', calculationSchema);
